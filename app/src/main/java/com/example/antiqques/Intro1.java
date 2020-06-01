d589a7af7677297665bdd5c28bb2b0036c046443
a7med <a7meddtareekk@gmail.com>
2020-06-01 17:11:39 +0200

    intro1 code

--- app/src/main/java/com/example/antiqques/Intro1.java
+++ app/src/main/java/com/example/antiqques/Intro1.java
@@ -1,14 +1,34 @@
 package com.example.antiqques;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Intro1 extends AppCompatActivity {
-
+    Button NextIntro1;
+    Button SkipIntro1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro1);
+        NextIntro1=findViewById(R.id.nextintro1_btn);
+        SkipIntro1=findViewById(R.id.skipintro1_btn);
+        NextIntro1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Intro1.this,Intro2.class);
+                startActivity(intent);
+            }
+        });
+        SkipIntro1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Intro1.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
