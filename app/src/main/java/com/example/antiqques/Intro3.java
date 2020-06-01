0ab5dc6b93141a622eb106246940a5ea56d3bc5e
HagerHifnawy <hageriprahim93@gmail.com>
2020-06-01 22:57:30 +0200

    intro3 code

--- app/src/main/java/com/example/antiqques/Intro3.java
+++ app/src/main/java/com/example/antiqques/Intro3.java
@@ -2,13 +2,33 @@ package com.example.antiqques;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Intro3 extends AppCompatActivity {
-
+    Button NextIntro3;
+    Button SkipIntro3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro3);
+        NextIntro3=findViewById(R.id.nextintro2_btn);
+        SkipIntro3=findViewById(R.id.skipintro2_btn);
+        NextIntro3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Intro3.this,Home.class);
+                startActivity(intent);
+            }
+        });
+        SkipIntro3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Intro3.this, Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
