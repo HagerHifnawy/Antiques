241ac811abf5e75013b30bd43a3b105e108c60a8
HagerHifnawy <hageriprahim93@gmail.com>
2020-06-02 06:13:50 +0200

    login code

--- app/src/main/java/com/example/antiqques/Intro3.java
+++ app/src/main/java/com/example/antiqques/Intro3.java
@@ -8,18 +8,18 @@ import android.view.View;
 import android.widget.Button;
 
 public class Intro3 extends AppCompatActivity {
-    Button NextIntro3;
+    Button finichIntro;
     Button SkipIntro3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro3);
-        NextIntro3=findViewById(R.id.nextintro2_btn);
+        finichIntro=findViewById(R.id.finichintro_btn);
         SkipIntro3=findViewById(R.id.skipintro2_btn);
-        NextIntro3.setOnClickListener(new View.OnClickListener() {
+        finichIntro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Intro3.this,Home.class);
+                Intent intent=new Intent(Intro3.this,Login.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/antiqques/Login.java
+++ app/src/main/java/com/example/antiqques/Login.java
@@ -41,5 +41,6 @@ public class Login extends AppCompatActivity {
                 startActivity(intent);
             }}
         });
+
     }
 }
