e47941da34f0d10ae3e6c224e7837785a109e275
HagerHifnawy <hageriprahim93@gmail.com>
2020-06-03 16:51:01 +0200

    forget ui

---
+++ app/src/main/java/com/example/antiqques/forgetpassword2.java
@@ -0,0 +1,14 @@
+package com.example.antiqques;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class forgetpassword2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgetpassword2);
+    }
+}
